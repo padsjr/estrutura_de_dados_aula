ab3077baa948f66b295ec52cb2b1be7afc836d11
padsjr <pedroadasilveirajr@hotmail.com>
2025-02-23 18:53:03 -0300

    fix

--- teste.c
+++ teste.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-#define I 25
+#define I 25/*para garantir menor uso de memoria melhor usar o define, os valores são substituídos diretamente no código sem armazenar variáveis na RAM*/
 #define J 80
 
 int main() {
 
+    /*
+    const int I = 25;
+    const int J = 80;
+    Dessa forma segue as boas praticas e teria mais segurança
+    */
     int resultado = I*J;
 
     printf("O valor de I é : %d\n", I);
