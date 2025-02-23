228e493c6aa609b0d6bc7d0774dae500e0f58d42
padsjr <pedroadasilveirajr@hotmail.com>
2025-02-23 18:33:54 -0300

    multiplicação de valores

---
+++ teste.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define I 25
+#define J 80
+
+int main() {
+
+    int resultado = I*J;
+
+    printf("O valor de I é : %d\n", I);
+    printf("O valor de J é : %d\n", J);
+    printf("O resultado da multiplicacao de J por I é : %d\n", resultado);
+
+
+    
+    return 0;
+} 
